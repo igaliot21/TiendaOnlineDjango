ddcead027e8ba420cc5f0fb1f4757e1b134ba50a
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-17 12:16:06 +0200

    Panel de Administración II

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gestionPedidos.models import Cliente, Articulos, Pedidos
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -2,21 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class Cliente(models.Model):
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
-    telefono=models.CharField(max_length=7)
 
-class Articulos(models.Model):
-    nombre=models.CharField(max_length=30)
-    seccion=models.CharField(max_length=20)
-    precio=models.IntegerField()
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    telefono = models.CharField(max_length=7)
 
     def __str__(self):
-        return 'El nombre es: %s la sección es: %s y el precio es: %s' % (self.nombre,self.seccion,self.precio)
+        return 'Nombre: %s, Dirección: %s, Email: %s, Teléfono: %s' % (self.nombre, self.direccion, self.email, self.telefono)
+
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return 'Nombre : %s, Sección: %s, Precio: %s' % (self.nombre, self.seccion, self.precio)
+
 
 class Pedidos(models.Model):
-    numero=models.IntegerField()
-    fecha=models.DateField()
-    entregado=models.BooleanField()
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
+
+    def __str__(self):
+        return 'Número: %s, Fecha: %s, Entregado: %s' % (self.numero, self.fecha, self.entregado)
