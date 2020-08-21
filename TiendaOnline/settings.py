e11d5bd343ef9978b992ce2975224ee0c6a139b9
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-21 12:45:54 +0200

    Formularios I

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TiendaOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["gestionPedidos/Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
+from django.template import Template, Context, loader
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+
+def buscar(request):
+    mensaje = "Artículo buscado: %r" % request.GET["prd"]
+    return HttpResponse(mensaje)
