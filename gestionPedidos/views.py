266a9b632aee8ac255cab1fb8dc837cdbdb0ac58
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-25 10:40:33 +0200

    Formularios II

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 from django.shortcuts import render
+from gestionPedidos.models import Articulos
 import datetime
 
 # Create your views here.
@@ -11,5 +12,12 @@ def busqueda_productos(request):
 
 
 def buscar(request):
-    mensaje = "Artículo buscado: %r" % request.GET["prd"]
+    if request.GET["prd"]:
+        #mensaje = "Artículo buscado: %r" % request.GET["prd"]
+        productoBuscar = request.GET["prd"]
+        articulos = Articulos.objects.filter(nombre__icontains=productoBuscar)
+        return render(request, "resultado_busqueda.html", {"articulos": articulos, "query": productoBuscar})
+    else:
+        mensaje = "Busqueda vacia"
+
     return HttpResponse(mensaje)
